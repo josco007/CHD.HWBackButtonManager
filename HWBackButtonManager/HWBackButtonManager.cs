9bb121c4256c7b4dfea5091c7a6872d9f5ea61f8
Noe miranda <noe_is10@msn.com>
2019-02-24 10:05:22 -0700

    Add project files.

---
+++ HWBackButtonManager/HWBackButtonManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CHD
+{
+    public class HWBackButtonManager 
+    {
+        public static readonly HWBackButtonManager Instance = new HWBackButtonManager();
+
+        public delegate bool OnBackButtonPressedDelegate();
+
+        private OnBackButtonPressedDelegate onBackButtonPressedListener;
+
+        private HWBackButtonManager()
+        {
+
+        }
+
+        public bool NotifyHWBackButtonPressed()
+        {
+
+            if (onBackButtonPressedListener != null)
+            {
+                return onBackButtonPressedListener.Invoke();
+            }
+
+            return true; 
+        }
+
+        public void SetHWBackButtonListener(OnBackButtonPressedDelegate onBackButtonPressedListener)
+        {
+            this.onBackButtonPressedListener = onBackButtonPressedListener;
+        }
+
+        public void RemoveHWBackButtonListener()
+        {
+            onBackButtonPressedListener = null;
+        } 
+    }
+}
